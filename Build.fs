a67327b0045a4b703e37c368a766de2b925dbb19
Jimmy Byrd <jimmybyrd@gmail.com>
2022-09-18 17:52:36 -0400

    Use RELEASE_NOTES to drive version numbers

--- Build.fs
+++ Build.fs
@@ -1,5 +1,6 @@
 open Fake.Core
 open Fake.IO
+open Fake.Core
 open Farmer
 open Farmer.Builders
 
@@ -15,6 +16,10 @@ let sharedTestsPath = Path.getFullName "tests/Shared"
 let serverTestsPath = Path.getFullName "tests/Server"
 let clientTestsPath = Path.getFullName "tests/Client"
 
+let release = ReleaseNotes.load "RELEASE_NOTES.md"
+
+let buildVersion = $"/p:Version={release.NugetVersion} /p:AssemblyVersion={release.NugetVersion}"
+
 Target.create "Clean" (fun _ ->
     Shell.cleanDir deployPath
     run dotnet "fable clean --yes" clientPath // Delete *.fs.js files created by Fable
@@ -23,7 +28,7 @@ Target.create "Clean" (fun _ ->
 Target.create "InstallClient" (fun _ -> run npm "install" ".")
 
 Target.create "Bundle" (fun _ ->
-    [ "server", dotnet $"publish -c Release -o \"{deployPath}\"" serverPath
+    [ "server", dotnet $"publish -c Release -o \"{deployPath}\" {buildVersion}" serverPath
       "client", dotnet "fable -o output -s --run npm run build" clientPath ]
     |> runParallel
 )
@@ -45,14 +50,14 @@ Target.create "Azure" (fun _ ->
 
 Target.create "Run" (fun _ ->
     run dotnet "build" sharedPath
-    [ "server", dotnet "watch run" serverPath
+    [ "server", dotnet $"watch run -- {buildVersion}" serverPath
       "client", dotnet "fable watch -o output -s --run npm run start" clientPath ]
     |> runParallel
 )
 
 Target.create "RunTests" (fun _ ->
     run dotnet "build" sharedTestsPath
-    [ "server", dotnet "watch run" serverTestsPath
+    [ "server", dotnet "watch run {buildVersion}" serverTestsPath
       "client", dotnet "fable watch -o output -s --run npm run test:live" clientTestsPath ]
     |> runParallel
 )
---
+++ RELEASE_NOTES.md
@@ -0,0 +1,5 @@
+## New in 2.0.0 (Released 2022/09/18)
+* Update to netcore 6.0
+
+## New in 1.0.0 (Released 2019/01/01)
+* Initial Release

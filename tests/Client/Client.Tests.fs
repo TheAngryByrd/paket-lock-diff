e3d79f4c1b274e1e163468498da59431d0e56121
Jimmy Byrd <jimmybyrd@gmail.com>
2023-09-16 11:15:39 -0400

    remove tests

--- tests/Client/Client.Tests.fs
+++ tests/Client/Client.Tests.fs
@@ -5,16 +5,16 @@ open Fable.Mocha
 open Index
 open Shared
 
-let client = testList "Client" [
-    testCase "Added todo" <| fun _ ->
-        let newTodo = Todo.create "new todo"
-        let model, _ = init ()
+// let client = testList "Client" [
+//     testCase "Added todo" <| fun _ ->
+//         let newTodo = Todo.create "new todo"
+//         let model, _ = init ()
 
-        let model, _ = update (AddedTodo newTodo) model
+//         let model, _ = update (AddedTodo newTodo) model
 
-        Expect.equal model.Todos.Length 1 "There should be 1 todo"
-        Expect.equal model.Todos.[0] newTodo "Todo should equal new todo"
-]
+//         Expect.equal model.Todos.Length 1 "There should be 1 todo"
+//         Expect.equal model.Todos.[0] newTodo "Todo should equal new todo"
+// ]
 
 let all =
     testList "All"
@@ -22,7 +22,7 @@ let all =
 #if FABLE_COMPILER // This preprocessor directive makes editor happy
             Shared.Tests.shared
 #endif
-            client
+            // client
         ]
 
 [<EntryPoint>]
